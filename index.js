5381e76b30bd3a115cc7f23ed344abf3e3709f6a
Albert Sikkema <github@albertsikkema.com>
2024-05-29 18:59:53 +0200

    Added two helper functions, to get more info/context about API and Frontend logging.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //index.js
 import {logger, backendlogger} from './src/logger.js'
+import {getInfoApi, getInfoFrontend} from './src/helper.js'
 
-
-export { logger, backendlogger} 
+export { logger, backendlogger, getInfoApi, getInfoFrontend} 
--- src/shared.js
+++ src/shared.js
@@ -1,3 +1,7 @@
+import crypto from 'crypto'
+
+export const isBrowser = typeof window !== 'undefined'
+
 export const throttle = (fn, wait) => {
   let lastFn, lastTime
   return function throttleFunction() {
@@ -19,4 +23,49 @@ export const throttle = (fn, wait) => {
   }
 }
 
-export const isBrowser = typeof window !== 'undefined'
+// used for keeping track of api interactions. Use it like this: logger.info('api - test', { info: getInfoApi(req), statusCode: 200 }).
+export function getInfoApi(request) {
+  const ip =
+    request.headers['cf-connecting-ip'] ||
+    request.headers['x-real-ip'] ||
+    request.headers['x-forwarded-for'] ||
+    request.headers['x-client-ip']
+
+  const customer_name = process.env.CUSTOMER_NAME
+  const host = process.env.URL
+
+  // Function to hash the IP address deterministically based on the date
+  const hashIpAddress = (ip) => {
+    const date = new Date()
+    // Get the current date in the format YYYYMMDD, this allows hashed IPs to be the same during 24hours.
+    const dateToday = date.toISOString().split('T')[0].replace(/-/g, '')
+    const dataToHash = `${ip}-${dateToday}`
+    const hash = crypto.createHash('sha256')
+    return hash.update(dataToHash).digest('hex')
+  }
+
+  const message = {
+    customer_name: customer_name,
+    host: host,
+    href: request.headers['x-invoke-path'],
+    query: request.query,
+    body: request.body,
+    ip: ip ? hashIpAddress(ip) : null,
+    'user-agent': request.headers['user-agent'],
+    referer: request.headers['referer'],
+    accept: request.headers['accept'],
+    'accept-encoding': request.headers['accept-encoding'],
+    'accept-language': request.headers['accept-language'],
+  }
+  return message
+}
+
+// used for keeping track of user interactions with the map. Use it f.i. like this: logger.info('Dataset Clicked', { action: `Dataset Clicked - ${state}`, mapId, datasetName, type: 'dataset', info: getInfoFrontend({}) })
+export function getInfoFrontend({ session }) {
+  const message = {
+    user: session?.user.email || null,
+    path: window.location.href,
+    host: window.location.hostname,
+  }
+  return message
+}
